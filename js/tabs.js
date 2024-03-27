76688459228af2b4767414dc6ca5266591bd6b75
Luciano Dias <mlucianodias@yahoo.com.br>
2024-03-27 11:09:37 -0300

    terminada tarefa modulo 11

---
+++ js/tabs.js
@@ -0,0 +1,17 @@
+function showsobrenos () {
+    document.getElementById('tab-sobrenos').style.display = 'block'
+    document.getElementById('tab-consultoria').style.display = 'none'
+    document.getElementById('tab-arquiteto').style.display = 'none'
+}
+
+function showconsultoria () {
+    document.getElementById('tab-sobrenos').style.display = 'none'
+    document.getElementById('tab-consultoria').style.display = 'block'
+    document.getElementById('tab-arquiteto').style.display = 'none'
+}
+
+function showarquiteto () {
+    document.getElementById('tab-sobrenos').style.display = 'none'
+    document.getElementById('tab-consultoria').style.display = 'none'
+    document.getElementById('tab-arquiteto').style.display = 'block'
+}
